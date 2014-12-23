e1bc6d5876ca7a214e5ac36f508d6f89b067a357
Joseph Hager <ajhager@gmail.com>
2014-12-23 15:43:11 -0500

    Various changes

--- src/repl.js
+++ src/repl.js
@@ -13,9 +13,9 @@ var pp = function(v) {
 
 	switch (v.constructor) {
 		case String:
-			return '"'+v+'"';
+			return '"' + v + '"';
 		case Array:
-			return "["+v.join(' ')+"]";
+			return "[" + v.join(' ') + "]";
 		default:
 			return v;
 	}
@@ -30,40 +30,110 @@ var DEFINITION = 1;
 var COMPILE = 2;
 var COMMENT = 3;
 var QUOTE = 4;
+var STRING = 5;
 
 var state = INTERPRET;
 
 var stack = [];
-var P = function(v) { stack.push(v); };
+var P = function(v) {
+	stack.push(v);
+};
 var name = "";
 var defstack = [];
 
 var dict = {
-	".": function(v1) { log(colors.green, pp(v1)); },
-	"+": function(v1, v2) { P(v1 + v2); },
-	"-": function(v1, v2) { P(v1 - v2); },
-	"*": function(v1, v2) { P(v1 * v2); },
-	"/": function(v1, v2) { P(v1 / v2); },
-	"%": function(v1, v2) { P(v1 % v2); },
-	"==": function(v1, v2) { P(v1 === v2); },
-	"!=": function(v1, v2) { P(v1 !== v2); },
-	"<": function(v1, v2) { P(v1 < v2); },
-	"<=": function(v1, v2) { P(v1 <= v2); },
-	">": function(v1, v2) { P(v1 > v2); },
-	">=": function(v1, v2) { P(v1 >= v2); },
-	"swap": function(v1, v2) { P(v2); P(v1); },
-	"dup": function(v1) { P(v1); P(v1); },
-	"dip": function(v1, v2) { exec(v2); P(v1);  },
-	"if": function(v1, v2, v3) { if (v1) { exec(v2); } else { exec(v3); } },
-	"over": function(v1, v2) { P(v1); P(v2); P(v1); },
-	"tuck": function(v1, v2) { P(v2); P(v1); P(v2); },
-	"rot": function(v1, v2, v3) { P(v2); P(v3); P(v1); },
-	"drop": function(v1) { },
-	"call": function(v1) { exec(v1); },
-	"compose": function(v1, v2) { P(v1.concat(v2)) },
-	"exit": function() { process.exit(); },
-	"clear": function() { stack = []; },
-	"Math.abs": function(v1) { P(Math.abs(v1)); },
+	".": function(v1) {
+		log(colors.green, pp(v1));
+	},
+	"+": function(v1, v2) {
+		P(v1 + v2);
+	},
+	"-": function(v1, v2) {
+		P(v1 - v2);
+	},
+	"*": function(v1, v2) {
+		P(v1 * v2);
+	},
+	"/": function(v1, v2) {
+		P(v1 / v2);
+	},
+	"%": function(v1, v2) {
+		P(v1 % v2);
+	},
+	"==": function(v1, v2) {
+		P(v1 === v2);
+	},
+	"!=": function(v1, v2) {
+		P(v1 !== v2);
+	},
+	"<": function(v1, v2) {
+		P(v1 < v2);
+	},
+	"<=": function(v1, v2) {
+		P(v1 <= v2);
+	},
+	">": function(v1, v2) {
+		P(v1 > v2);
+	},
+	">=": function(v1, v2) {
+		P(v1 >= v2);
+	},
+	"swap": function(v1, v2) {
+		P(v2);
+		P(v1);
+	},
+	"dup": function(v1) {
+		P(v1);
+		P(v1);
+	},
+	"dip": function(v1, v2) {
+		exec(v2);
+		P(v1);
+	},
+	"if": function(v1, v2, v3) {
+		if (v1) {
+			exec(v2);
+		} else {
+			exec(v3);
+		}
+	},
+	"over": function(v1, v2) {
+		P(v1);
+		P(v2);
+		P(v1);
+	},
+	"tuck": function(v1, v2) {
+		P(v2);
+		P(v1);
+		P(v2);
+	},
+	"rot": function(v1, v2, v3) {
+		P(v2);
+		P(v3);
+		P(v1);
+	},
+	"drop": function(v1) {},
+	"call": function(v1) {
+		exec(v1);
+	},
+	"compose": function(v1, v2) {
+		P(v1.concat(v2))
+	},
+	"exit": function() {
+		process.exit();
+	},
+	"clear": function() {
+		stack = [];
+	},
+	"eval": function(v1) {
+		eval(v1);
+	},
+	"def": function(v1, v2) {
+		eval('dict["' + v2 + '"] = ' + v1);
+	},
+	"Math.abs": function(v1) {
+		P(Math.abs(v1));
+	},
 };
 
 var kernel = [
@@ -89,7 +159,7 @@ var isString = function(val) {
 
 var call = function(word) {
 	var argc = word.length
-	var argv = stack.splice(stack.length-argc, argc);
+	var argv = stack.splice(stack.length - argc, argc);
 	if (argv.length == argc) {
 		word.apply(null, argv);
 	} else {
@@ -101,7 +171,7 @@ var call = function(word) {
 var execute = function(tokens) {
 	var tokens = tokens || [];
 
-	for (var i=0; i < tokens.length; i++) {
+	for (var i = 0; i < tokens.length; i++) {
 		var token = tokens[i];
 
 		switch (state) {
@@ -120,36 +190,36 @@ var execute = function(tokens) {
 							return error;
 						}
 					}
-				} else if (token == 'null') {
+				} else if (token === 'null') {
 					stack.push(null);
-				} else if (token == 'undefined') {
+				} else if (token === 'undefined') {
 					stack.push(undefined);
-				} else if (token == 'true') {
+				} else if (token === 'true') {
 					stack.push(true);
-				} else if (token == 'false') {
+				} else if (token === 'false') {
 					stack.push(false);
 				} else if (isNum(token)) {
 					stack.push(Number(token));
-				} else if (isString(token)) {
-					stack.push(token.slice(1, -1));
+				} else if (token === '"') {
+					state = STRING;
 				} else if (token === ':') {
 					state = DEFINITION;
 				} else if (token === '(') {
 					state = COMMENT;
 				} else if (token === "[") {
-					state = QUOTE;	
+					state = QUOTE;
 				} else {
 					return "UNKNOWN TOKEN: " + token;
 				}
 				break;
 			case DEFINITION:
-				name = token;	
+				name = token;
 				state = COMPILE;
 				break;
 			case COMPILE:
 				if (token === ":") {
 					return "TOKEN : INSIDE DEFINITION";
-				}	else if (token === ";") {
+				} else if (token === ";") {
 					if (dict.hasOwnProperty(name)) {
 						return "DUPLICATE WORD: " + name;
 					}
@@ -165,10 +235,19 @@ var execute = function(tokens) {
 					state = INTERPRET;
 				}
 				break;
+			case STRING:
+				if (token === '"') {
+					stack.push(defstack.join(' '));
+					defstack = [];
+					state = INTERPRET;
+				} else {
+					defstack.push(token);
+				}
+				break;
 			case QUOTE:
 				if (token === "[") {
 					return "TOKEN [ INSIDE QUOTATION";
-				}	else if (token === "]") {
+				} else if (token === "]") {
 					stack.push(defstack);
 					defstack = [];
 					state = INTERPRET;
@@ -207,14 +286,14 @@ module.exports = function() {
 		if (state === INTERPRET) {
 			repl.setPrompt('catcus> ');
 			var pstack = [];
-			for (var i=0; i < stack.length; i++) {
+			for (var i = 0; i < stack.length; i++) {
 				pstack.push(pp(stack[i]));
 			}
 			log(colors.white.bold, '=>', pstack.join(' '));
 		} else {
 			repl.setPrompt(colors.yellow('     .. '), 8);
 		}
-		
+
 		repl.prompt();
 	});
 
