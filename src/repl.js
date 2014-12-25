b4994dcc071644e79e1a60bba8b587bc2640cdd6
Joseph Hager <ajhager@gmail.com>
2014-12-25 14:16:38 -0500

    Refactor code

--- src/repl.js
+++ src/repl.js
@@ -1,4 +1,5 @@
 var colors = require('colors/safe');
+var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
 
@@ -15,9 +16,20 @@ var stackToString = function() {
 var banner = colors.grey("  /\\___/\\\n  ) -.- (  " + colors.white.bold("$%#@&!\n") + " =\\  o  /=   " + colors.yellow("v0.0.1\n") + "   )   (\n");
 
 module.exports = function() {
+	var oldLog = console['log'].bind(console);
+	console['log'] = function() {
+		var args = [].slice.call(arguments, 0);
+		oldError(colors.green(args.join(' ')));	
+	};
+
+	var oldError = console['error'].bind(console);
+	console['error'] = function() {
+		var args = [].slice.call(arguments, 0);
+		oldError(colors.red(args.join(' ')));	
+	};
+
 	console.log(banner);
 
-	var fs = require('fs');
 	var kernel = fs.readFileSync(path.dirname(__dirname) + '/lib/kernel.cus', 'utf8');
 	runtime.exec(kernel.match(/\S+/g));
 
@@ -39,7 +51,11 @@ module.exports = function() {
 			repl.setPrompt(blockPrompt, 8);
 		} else {
 			repl.setPrompt(normalPrompt, 8);
-			console.log(colors.white.bold('=>'), colors.white.bold(stackToString()));
+			var output = [];
+			for (var i = 0; i < runtime.stack.length; i++) {
+				output.push(runtime.print(runtime.stack[i]));
+			}
+			console.log(colors.white.bold('=>'), colors.white.bold(output.join(' ')));
 		}
 
 		repl.prompt();
--- src/runtime.js
+++ src/runtime.js
@@ -1,5 +1,3 @@
-var colors = require('colors/safe');
-
 var Quotation = function(tokens) {
 	for (var i = 0; i < tokens.length; i++) {
 		this.push(tokens[i]);
@@ -10,147 +8,6 @@ Quotation.prototype.constructor = Quotation;
 
 Quotation.prototype.toString = function() {
 	return "[" + this.join(' ') + "]";
-};
-
-var q = new Quotation([]);
-
-var parser;
-var parsers = {
-	'(': function() {
-		this.parse = function(token) {
-			if (token === ")") {
-				return true;
-			}
-		};
-	},
-
-	'"': function() {
-		this.stack = [];
-
-		this.parse = function(token) {
-			if (token === '"') {
-				runtime.push(this.stack.join(' '));
-				return true;
-			} else {
-				this.stack.push(token);
-			}
-		};
-	},
-
-	'[': function() {
-		this.stack = [];
-		this.depth = 1;
-
-		this.parse = function(token) {
-			if (token === "[") {
-				this.depth += 1;
-				this.stack.push(token);
-			} else if (token === "]") {
-				this.depth -= 1;
-				if (this.depth === 0) {
-					runtime.push(new Quotation(this.stack));
-					return true;
-				} else {
-					this.stack.push(token);
-				}
-			} else {
-				this.stack.push(token);
-			}
-		};
-	},
-
-	':': function() {
-		this.stack = [];
-
-		this.parse = function(token) {
-			if (token === ":") {
-				throw "TOKEN : INSIDE DEFINITION";
-			} else if (token === ";") {
-				if (this.stack.length === 0) {
-					return true;
-				}
-
-				var name = this.stack.shift();
-
-				if (runtime.lookup(name)) {
-					throw "DUPLICATE WORD: " + name;
-				}
-
-				runtime.define(name, this.stack);
-				return true;
-			} else {
-				this.stack.push(token);
-			}
-		};
-	},
-
-	'JS:': function() {
-		this.stack = [];
-
-		this.parse = function(token) {
-			if (token === ";") {
-				if (this.stack.length === 0) {
-					return true;
-				}
-
-				var name = this.stack.shift();
-
-				if (runtime.lookup(name)) {
-					throw "DUPLICATE WORD: " + name;
-				}
-
-				eval('runtime.define("' + name + '", ' + this.stack.join(' ') + ')');
-
-				return true;
-			} else {
-				this.stack.push(token);
-			}
-		};
-	},
-};
-
-var execute = function(tokens) {
-	var tokens = tokens || [];
-
-	for (var i = 0; i < tokens.length; i++) {
-		var token = tokens[i];
-
-		if (parser) {
-			try {
-				var result = parser.parse(token);
-				if (result) {
-					parser = null;
-				}
-			} catch (e) {
-				return e;
-			}
-		} else {
-			if (token.constructor === Quotation) {
-				runtime.push(token)
-				continue;
-			}
-			var word = runtime.lookup(token);
-			if (word) {
-				if (typeof(word) === 'function') {
-					var error = runtime.call(word);
-					if (error) {
-						return error;
-					}
-				} else {
-					var error = runtime.exec(word);
-					if (error) {
-						return error;
-					}
-				}
-			} else if (parsers[token]) {
-				parser = new parsers[token];
-			} else if (!isNaN(token)) {
-				runtime.push(Number(token));
-			} else {
-				return "UNKNOWN TOKEN: " + token;
-			}
-		}
-	}
 };
 
 var runtime = {
@@ -166,7 +23,7 @@ var runtime = {
 	},
 
 	parsing: function() {
-		return parser;
+		return this.parser;
 	},
 
 	namespace: {},
@@ -174,13 +31,7 @@ var runtime = {
 		this.namespace[word] = func;
 	},
 	lookup: function(word) {
-		var obj = this.namespace[word];
-		if (obj) return obj;
-
-		var s = word.split('.')
-		obj = global[s.shift()];
-		while (obj && s.length) obj = obj[s.shift()];
-		return obj;
+		return this.namespace[word];
 	},
 	call: function(word) {
 		var argc = word.length
@@ -199,19 +50,158 @@ var runtime = {
 			return "stack underflow";
 		}
 	},
-	exec: function(quote) {
-		var error = execute(quote);
-		if (error) {
-			console.log(colors.red(error));
+
+	parser: null,
+	parsers: {
+		'(': function() {
+			this.parse = function(token) {
+				if (token === ")") {
+					return true;
+				}
+			};
+		},
+
+		'"': function() {
+			this.stack = [];
+
+			this.parse = function(token) {
+				if (token === '"') {
+					runtime.push(this.stack.join(' '));
+					return true;
+				} else {
+					this.stack.push(token);
+				}
+			};
+		},
+
+		'[': function() {
+			this.stack = [];
+			this.depth = 1;
+
+			this.parse = function(token) {
+				if (token === "[") {
+					this.depth += 1;
+					this.stack.push(token);
+				} else if (token === "]") {
+					this.depth -= 1;
+					if (this.depth === 0) {
+						runtime.push(new Quotation(this.stack));
+						return true;
+					} else {
+						this.stack.push(token);
+					}
+				} else {
+					this.stack.push(token);
+				}
+			};
+		},
+
+		':': function() {
+			this.stack = [];
+
+			this.parse = function(token) {
+				if (token === ":") {
+					throw "TOKEN : INSIDE DEFINITION";
+				} else if (token === ";") {
+					if (this.stack.length === 0) {
+						return true;
+					}
+
+					var name = this.stack.shift();
+
+					if (runtime.lookup(name)) {
+						throw "DUPLICATE WORD: " + name;
+					}
+
+					runtime.define(name, this.stack);
+					return true;
+				} else {
+					this.stack.push(token);
+				}
+			};
+		},
+
+		'JS:': function() {
+			this.stack = [];
+
+			this.parse = function(token) {
+				if (token === ";") {
+					if (this.stack.length === 0) {
+						return true;
+					}
+
+					var name = this.stack.shift();
+
+					if (runtime.lookup(name)) {
+						throw "DUPLICATE WORD: " + name;
+					}
+
+					eval('runtime.define("' + name + '", ' + this.stack.join(' ') + ')');
+
+					return true;
+				} else {
+					this.stack.push(token);
+				}
+			};
+		},
+	},
+
+	exec: function(tokens) {
+		var tokens = tokens || [];
+
+		for (var i = 0; i < tokens.length; i++) {
+			var token = tokens[i];
+
+			if (this.parser) {
+				try {
+					var result = this.parser.parse(token);
+					if (result) {
+						this.parser = null;
+					}
+				} catch (e) {
+					console.error(e);
+					return;
+				}
+			} else {
+				if (token.constructor === Quotation) {
+					runtime.push(token)
+					continue;
+				}
+				var word = runtime.lookup(token);
+				if (word) {
+					if (typeof(word) === 'function') {
+						var error = runtime.call(word);
+						if (error) {
+							console.error(error);
+							return;
+						}
+					} else {
+						var error = runtime.exec(word);
+						if (error) {
+							console.error(error);
+							return;
+						}
+					}
+				} else if (this.parsers[token]) {
+					this.parser = new this.parsers[token];
+				} else if (!isNaN(token)) {
+					runtime.push(Number(token));
+				} else {
+					console.error("UNKNOWN TOKEN: " + token);
+					return;
+				}
+			}
 		}
 	},
 
 	print: function(v) {
 		if (typeof v === 'string') {
 			return '"' + v + '"';
+		} else if (Array.isArray(v)) {
+			return "[" + v.join(' ') + "]"
 		}
 		return String(v);
-	},
+	}
 };
 
 module.exports = runtime;
